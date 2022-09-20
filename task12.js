5547202edb6e3fa14e68b28b17e12ed948d72b21
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-20 17:03:14 +0300

    12 tasks

---
+++ task12.js
@@ -0,0 +1,11 @@
+const table = [];
+
+for (let i = 1; i <= 10; i++) {
+  const subArr = [];
+  for (let j = 1 + (i - 1) * 10; j <= i * 10; j++) {
+    subArr.push(j * j);
+  }
+  table.push(subArr);
+}
+
+console.log(table);
