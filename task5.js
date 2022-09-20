e81c3c10e492bbd4341947f0a12dca7360cf4191
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-20 12:35:48 +0300

    5 tasks

---
+++ task5.js
@@ -0,0 +1,12 @@
+const arr = [{a: 5, b: 3, c: 4}, {a: 1, b: 3, c: 4}, {a: 4, b: 7, c: 4}, {a: 2, b: 2, c: 3}]
+const perimeter = (arr) => {
+    const newArr = []
+arr.forEach(elem => {
+    const newItem = {
+        p:elem.a+elem.b+elem.c
+    }
+    newArr.push(newItem)
+});
+return newArr
+}
+console.log(perimeter(arr));
