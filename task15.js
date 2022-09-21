a952cb1f0ab94a5c85611ea51531d1e1cc10eff7
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-21 11:32:15 +0300

    15 tasks

---
+++ task15.js
@@ -0,0 +1,12 @@
+const arr = [1, 2, 3, 4, 5];
+const filter = (el) => el % 2 == 0;
+const trasform = (el) => el * 2;
+
+const result = (arr, filter, trasform) => {
+  filteredArr = [];
+  
+    arr.forEach((item)=>{ if(filter(item)) { filteredArr.push(item)}})
+  
+  filteredArr.forEach((item)=>console.log(trasform(item)))
+};
+result(arr, filter, trasform);
