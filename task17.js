98c982acc3cecf57aa492c8e5644ad111f0c56c3
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-21 14:07:20 +0300

    17,18 tasks

--- task17.js
+++ task17.js
@@ -1,10 +1,11 @@
 //В функцию подается несколько массивов. Вернуть один массив со всеми элементами.
 
-const arr = []
-const subArr1 = [1,2,3]
-const subArr2 = [4,5,6]
+const subArr1 = [1, 2, 3];
+const subArr2 = [4, 5, 6];
 const concatArr = (...subArrays) => {
-    
-    subArrays.forEach( (item)=> console.log([...item]))
-}
-console.log(concatArr(subArr1,subArr2));
+  let arr = [];
+  subArrays.forEach((item) => (arr = [...arr, ...item]));
+  return arr;
+};
+
+console.log(concatArr(subArr1, subArr2));
