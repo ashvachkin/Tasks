20b54dd1eb483724d8fe1815716e3bbf5416ee9a
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-26 12:54:06 +0300

    22 fixed

--- task22.js
+++ task22.js
@@ -2,8 +2,20 @@
 //Функция должна возвращать массив фрагмент массива array, начиная с индекса start и заканчивая end.
 
 const arr = [12, 2, 37, 44, 5, 6, 71, 8, 9, 111111];
+
 const arrSplit = (arr, start, end) => {
-  return (slicedArr = arr.slice(start, end));
+  let result = [];
+
+  if (!end || end > arr.length) {
+    for (let i = start; i < arr.length; i++) {
+      result.push(arr[i]);
+    }
+  } else {
+    for (let i = start; i < end; i++) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
 
-console.log(arrSplit(arr, 0, 7));
+console.log(arrSplit(arr, 2, 7));
