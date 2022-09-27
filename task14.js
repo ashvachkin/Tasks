7dda2b53cc82259a95e182648602b67c19cb3029
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-27 17:21:32 +0300

    14 finished

--- task14.js
+++ task14.js
@@ -1,15 +1,20 @@
 //Даны две строки. Напишите функцию, которая определит, содержится ли меньшая по длине строка в большей.
-const firstStr = "text education part 2";
+
+const firstStr = "tet ceducation on part 2 par";
 const secondStr = "text";
-
+const thirdStr = "on par";
+const fourthStr = "cat";
 const findSecond = (str1, str2) => {
-  let splitedStr = [];
-  splitedStr = str1.split(" ");
-  for (let i = 0; i < splitedStr.length; i++) {
-    if (splitedStr[i] === str2.split()[0]) {
-      return true;
-    } else return false;
+  let flag = false;
+  for (let j = 0; j < str1.length; j++) {
+    if (str2[0] === str1[j]) {
+      let temp = true;
+      for (let g = 1; g < str2.length; g++) {
+        temp = temp && str2[g] === str1[j + g];
+      }
+      flag = temp;
+    }
   }
+  return flag;
 };
-
 console.log(findSecond(firstStr, secondStr));
