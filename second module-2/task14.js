c7b1895f589f50ac357d7c3b7666bf5bb3d42c54
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-28 18:18:11 +0300

    14 fixed

--- second module-2/task14.js
+++ second module-2/task14.js
@@ -1,14 +1,17 @@
 //Реализуйте функцию, котора параметром принимает строку. Выведите сформированный объект из параметров строки браузера.
 
-const str = "https://underscorejs.org?a=4&b=8";
-const str2 = "https://underscorejs.org?id=123&limit=5&offset=0";
+const str = "https://underscorejs.org?";
+const str2 = "https://underscorejs.org?id=123&limit=5&offset=a";
 const createObj = (str) => {
-  let newObj = {};
+  const newObj = {};
   let firstSplit = str.split("?");
+  if (!firstSplit[1]) {
+    return {};
+  }
   let secondSplit = firstSplit[1].split("&");
   for (let i = 0; i < secondSplit.length; i++) {
     let thirdSplit = secondSplit[i].split("=");
-    newObj[thirdSplit[0]] = +thirdSplit[1];
+    newObj[thirdSplit[0]] = thirdSplit[1];
   }
   return newObj;
 };
