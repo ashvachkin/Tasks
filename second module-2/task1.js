9dc394b5e18aaded938d2d9e06638585f1ee4f14
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-28 15:03:54 +0300

    1 fixed

--- second module-2/task1.js
+++ second module-2/task1.js
@@ -3,7 +3,7 @@
 const arr = [1, 3, 123, 2, 12, -123, 1233];
 
 const sortArr = (arr, way) => {
-  if (way === "asc") {
+  if (way) {
     for (let j = arr.length - 1; j > 0; j--) {
       for (let i = 0; i < j; i++) {
         if (arr[i] > arr[i + 1]) {
@@ -13,17 +13,10 @@ const sortArr = (arr, way) => {
         }
       }
     }
-  } else if (way === "desc") {
-    for (let j = arr.length - 1; j > 0; j--) {
-      for (let i = 0; i < j; i++) {
-        if (arr[i] < arr[i + 1]) {
-          let temp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = temp;
-        }
-      }
-    }
   }
-  return arr;
+  if (way === "desc") {
+    arr.reverse();
+  }
+  if (way === "asc") return arr;
 };
-console.log(sortArr(arr, "desc"));
+console.log(sortArr(arr, "asc"));
