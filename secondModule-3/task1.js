c2de37598bb53e21844bdff711c438d46769c79e
Artem Shvachkin <ashvachkin.itrum@gmail.com>
2022-09-29 11:02:19 +0300

    1 finished

--- secondModule-3/task1.js
+++ secondModule-3/task1.js
@@ -1,22 +1,11 @@
 //Напишите функцию, которая добавит в первый массив только те значения, которые присутствуют во втором, но нет в первом
 
 const arr = [5, 8, -3];
-const arr2 = [8, 5, 7, -3];
-let index = 0;
+const arr2 = [8, 7, -3, 1, 2];
 const checkArr = (arr1, arr2) => {
-  let newArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    let flag = false;
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] !== arr2[j]) {
-        flag = true;
-        index = j;
-      }
-    }
-    if (flag === true) {
-      newArr.push(arr2[index]);
-    }
-  }
-  return newArr;
+  let newArr = arr1.concat(arr2)
+  let filteredArr = [...new Set(newArr)]
+  return filteredArr;
 };
+
 console.log(checkArr(arr, arr2));
